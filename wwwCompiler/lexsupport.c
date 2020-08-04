a78c872d928c7d3282f996508d9e49fc474c6847
Matthew Hill <mh1619@ic.ac.uk>
2020-08-04 16:19:27 +0100

    implemented compiling for wave declarations

--- wwwCompiler/lexsupport.c
+++ wwwCompiler/lexsupport.c
@@ -1,4 +1,5 @@
 #include "lexsupport.h"
+#include <string.h>
 
 YYSTYPE yylval; //token value
 
@@ -6,6 +7,9 @@ void print_token(FILE *out, int tok) {
   switch (tok) {
   case NUMBER:
     fprintf(out, "number(%d)", yylval.n);
+    break;
+  case FLOAT:
+    fprintf(out, "float(%lf)", yylval.d);
     break;
   case MIDI_FREQUENCY:
     fputs("midi.frequency", out);
@@ -40,9 +44,12 @@ void print_token(FILE *out, int tok) {
   case WAVE_RELEASE:
     fputs("release", out);
     break;
+  case OUTPUT_WAVE:
+    fputs("output select", out);
+    break;
   case INVALID:
     fputs("invalid syntax", out);
-    break;
+    break;  
   default:
     fprintf(out, "unknown token %d", tok);
   }
@@ -51,3 +58,91 @@ void print_token(FILE *out, int tok) {
 int yywrap(void) {
   return 1;
 }
+/* searches for a wave name in the list of wave names
+   returns 0 if nothing found and 1 otherwise
+   key - the wave name being searched for
+   wave_names - the wave names list being searched */
+static int search_for_wave(char *key, char *wave_names[MAX_WAVES]) {
+  for(int i = 0; i < MAX_WAVES; i++) {
+    if (!wave_names[i]) {
+      return 0;
+    }
+    if (strcmp(wave_names[i], key) == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+static int create_wave(char *name, char *wave_names[MAX_WAVES]) {
+  for (int i = 0; i < MAX_WAVES; i++) {
+    if (!wave_names[i]) {
+      wave_names[i] = name;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void tok_from_start(www_state *state, int tok, FILE *out, char *wave_names[MAX_WAVES]) {
+  switch (tok) {
+  case WAVE_IDENTIFIER:
+    if (search_for_wave(yylval.s, wave_names)) {
+      *state = ATTRIBUTE; //attribute adjustment expected
+      fprintf(out, "%s", yylval.s); //starts line for an attribute adjustment 
+    } else {
+      if (create_wave(yylval.s, wave_names)) {
+	//state remains start as wave creation is successful
+	//creates a memory alloation for the wave
+	fprintf(out, "Wave *%s = (Wave *) malloc(sizeof(Wave));\n", yylval.s);
+      } else {
+	//too many waves error
+	printf("ERROR: wave limit %d exceeded", MAX_WAVES);
+	*state = ERROR;
+      }
+    }
+    break;
+  case OUTPUT_WAVE:
+    //token selecting output wave detected
+    *state = SELECT;
+    break;
+  default:
+    //invalid token
+    printf("ERROR: Invalid syntax");
+    *state = ERROR;
+  }
+}
+
+void tok_from_select(www_state *state, int tok, FILE *out,  char *wave_names[MAX_WAVES], char **played_wave) {
+  switch (tok) {
+  case WAVE_IDENTIFIER:
+    if (search_for_wave(yylval.s, wave_names)) {
+      *played_wave = yylval.s;
+      *state = START; //wave initialised and state reset
+    } else {
+      if (create_wave(yylval.s, wave_names)) {
+	*played_wave = yylval.s;
+	fprintf(out, "Wave *%s = (Wave *) malloc(sizeof(Wave));\n", yylval.s);
+	*state = START; //state reset for next line
+      } else {
+	//too many waves error
+	printf("ERROR: wave limit %d exceeded", MAX_WAVES);
+	*state = ERROR;
+      }
+    }
+    break;
+  default:
+    //invalid token
+    *state = ERROR;
+    printf("ERROR: Invalid syntax");
+  }
+}
+
+void tok_from_attribute(www_state *state, int tok, FILE *out) {
+
+}
+
+void tok_from_specific_attribute(www_state *state, int tok, FILE *out,  char *wave_names[MAX_WAVES]) {
+
+}
+
--- wwwCompiler/lexsupport.h
+++ wwwCompiler/lexsupport.h
@@ -3,28 +3,59 @@
 
 #include <stdio.h>
 
+/* maximum number of waves in one program */
+#define MAX_WAVES (10)
+
 /* Tokens */
 #define NUMBER (1)
-#define MIDI_FREQUENCY (2)
-#define MIDI_VELOCITY (3)
-#define WAVE_IDENTIFIER (4)
-#define WAVE_BASE (5)
-#define WAVE_AMPLITUDE (6)
-#define WAVE_FREQUENCY (7)
-#define WAVE_PHASE (8)
-#define WAVE_ATTACK (9)
-#define WAVE_DECAY (10)
-#define WAVE_SUSTAIN (11)
-#define WAVE_RELEASE (12)
+#define FLOAT (2)
+#define MIDI_FREQUENCY (3)
+#define MIDI_VELOCITY (4)
+#define WAVE_IDENTIFIER (5)
+#define WAVE_BASE (6)
+#define WAVE_AMPLITUDE (7)
+#define WAVE_FREQUENCY (8)
+#define WAVE_PHASE (9)
+#define WAVE_ATTACK (10)
+#define WAVE_DECAY (11)
+#define WAVE_SUSTAIN (12)
+#define WAVE_RELEASE (13)
+#define OUTPUT_WAVE (14)
 #define INVALID (-1)
 
 typedef union {
   int n;
+  double d;
   char *s;
 } YYSTYPE;
 
+/* state of compiler when running through tokens */
+typedef enum compiler_state {
+  START,            //nothing to know previously
+  SELECT,           //selecting an outputted wave
+  ATTRIBUTE,        //adjusting an attribute
+  BASE_SELECT,      //adjusting base
+  FREQUENCY_SELECT, //adjusting frequency
+  AMPLITUDE_SELECT, //adjusting amplitude
+  PHASE_SELECT,     //adjusting phase
+  ATTACK_SELECT,    //adjusting attack
+  DECAY_SELECT,     //adjusting decay
+  SUSTAIN_SELECT,   //adjusting sustain
+  RELEASE_SELECT,   //adjusting release
+  ERROR,            //compile time error detected
+} www_state;
+
 extern YYSTYPE yylval; //token value
+extern FILE *yyin, *yyout;
 
 void print_token(FILE *out, int tok);
+
+void tok_from_start(www_state *state, int tok, FILE *out, char *wave_names[MAX_WAVES]);
+
+void tok_from_select(www_state *state, int tok, FILE *out,  char *wave_names[MAX_WAVES], char **played_wave);
+
+void tok_from_attribute(www_state *state, int tok, FILE *out);
+
+void tok_from_specific_attribute(www_state *state, int tok, FILE *out,  char *wave_names[MAX_WAVES]);
 
 #endif
